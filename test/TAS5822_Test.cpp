7aac62376132d485ac161ed4c297d27385ea51be
Chris <chris@chrisbarrbuilds.com>
2024-02-05 20:05:27 +1030

    Rework RegisterModel

--- test/TAS5822_Test.cpp
+++ test/TAS5822_Test.cpp
@@ -18,8 +18,18 @@ class RegisterModel {
 public:
     RegisterModel(uint8_t addr) : addr(addr) { reset(); }
 
+    struct Register {
+        uint8_t value{0};
+        int writeCount{0};
+        int readCount{0};
+    };
+
     void reset() {
         registers.clear();
+        registers.reserve(regMax);
+
+        for (int i = 0; i < regMax; i++) { registers.push_back(Register()); }
+
         active = false;
         writeIndex = 0;
         targetReg = 0;
@@ -61,7 +71,8 @@ public:
         }
         case 1: {
             // std::cout << "Writing Register: " << int(targetReg) << " = " << int(val) << "\n";
-            registers[targetReg] = val;
+            registers[targetReg].value = val;
+            registers[targetReg].writeCount += 1;
             writeIndex = 0;
             break;
         }
@@ -71,45 +82,57 @@ public:
 
     uint8_t read() {
         if (!active) { return false; }
-
-        if (registers.count(targetReg) != 0) {
-            // std::cout << "Reading Register: " << int(targetReg) << " = " << int(registers[targetReg]) << "\n";
-            return registers[targetReg];
-        } else {
-            return 0;
-        }
+        // std::cout << "Reading Register: " << int(targetReg) << " = " << int(registers[targetReg]) << "\n";
+        registers[targetReg].readCount += 1;
+        return registers[targetReg].value;
     }
 
-    void registerExistsAndHasValue(TAS5822::Register reg, uint8_t value, uint8_t bitmask = 0xFF) {
-        uint8_t regInt = static_cast<uint8_t>(reg);
-        bool hasRegister = registers.count(regInt);
-        EXPECT_EQ(true, hasRegister) << "Register " << std::hex << int(regInt) << " was not written.";
-        if (hasRegister) {
-            uint8_t actualMasked = (registers[regInt] & bitmask);
-            uint8_t expectedMasked = (value & bitmask);
-            EXPECT_EQ(expectedMasked, actualMasked)
-                << "Register " << std::hex << int(regInt) << " Actual " << std::bitset<8>(actualMasked)
-                << " != " << std::bitset<8>(expectedMasked) << " Expected";
-        }
+    // management methods
+    void setRegister(TAS5822::Register reg, uint8_t value) { registers[static_cast<uint8_t>(reg)].value = value; }
+
+    const Register& getRegister(TAS5822::Register reg) const { return registers[static_cast<uint8_t>(reg)]; }
+
+    const int getTotalRegisterWriteCount() {
+        int count = 0;
+        for (const auto& reg : registers) { count += reg.writeCount; }
+        return count;
     }
 
-    std::map<uint8_t, uint8_t> registers;
+    const int getTotalRegisterReadCount() {
+        int count = 0;
+        for (const auto& reg : registers) { count += reg.writeCount; }
+        return count;
+    }
 
 private:
+    const uint8_t regMax = 255;
+    std::vector<Register> registers;
     uint8_t addr;
     bool active = false;
     int writeIndex = 0;
     uint8_t targetReg = 0;
 };
 
+void testRegisterWasWritten(RegisterModel& m, TAS5822::Register reg) {
+    int writeCount = m.getRegister(reg).writeCount;
+    EXPECT_GT(writeCount, 0) << "Register " << std::hex << static_cast<int>(reg) << " write count " << writeCount
+                             << " !> 0";
+}
+
+void testRegisterHasValue(RegisterModel& m, TAS5822::Register reg, uint8_t value, uint8_t bitmask = 0xFF) {
+    uint8_t actualMasked = (m.getRegister(reg).value & bitmask);
+    uint8_t expectedMasked = (value & bitmask);
+    EXPECT_EQ(expectedMasked, actualMasked)
+        << "Register " << std::hex << static_cast<int>(reg) << " Actual " << std::bitset<8>(actualMasked)
+        << " != " << std::bitset<8>(expectedMasked) << " Expected";
+}
+
 class RegisterModelTest : public testing::Test {
 protected:
     const uint8_t defaultAddress{0x44};
 
     RegisterModelTest() : regmodel(defaultAddress) {}
-    void SetUp() override {
-        When(Method(ArduinoFake(), delay)).AlwaysReturn();
-    }
+    void SetUp() override { When(Method(ArduinoFake(), delay)).AlwaysReturn(); }
 
     RegisterModel regmodel;
 };
@@ -118,13 +141,14 @@ protected:
 TEST_F(RegisterModelTest, BasicRegisterWritePattern) {
     TAS5822::TAS5822<RegisterModel> amp(regmodel, defaultAddress, -1);
     amp.writeRegister(TAS5822::Register::DEVICE_CTRL_2, 123);
-    regmodel.registerExistsAndHasValue(TAS5822::Register::DEVICE_CTRL_2, 123);
+    testRegisterWasWritten(regmodel, TAS5822::Register::DEVICE_CTRL_2);
+    testRegisterHasValue(regmodel, TAS5822::Register::DEVICE_CTRL_2, 123);
 }
 
 // Check that readRegister gets value
 TEST_F(RegisterModelTest, BasicRegisterReadPattern) {
     TAS5822::TAS5822<RegisterModel> amp(regmodel, defaultAddress, -1);
-    regmodel.registers[static_cast<uint8_t>(TAS5822::Register::DEVICE_CTRL_2)] = 121;
+    regmodel.setRegister(TAS5822::Register::DEVICE_CTRL_2, 121);
     EXPECT_EQ(static_cast<uint8_t>(121), amp.readRegister(TAS5822::Register::DEVICE_CTRL_2));
 }
 
@@ -134,11 +158,16 @@ TEST_F(RegisterModelTest, DefaultInitialisedState) {
     TAS5822::TAS5822<RegisterModel> amp(regmodel, defaultAddress, -1);
 
     // check that constructor writes to no registers
-    EXPECT_EQ(static_cast<uint8_t>(0), regmodel.registers.size());
+    EXPECT_EQ(0, regmodel.getTotalRegisterWriteCount());
+    EXPECT_EQ(0, regmodel.getTotalRegisterReadCount());
 
     amp.begin();
 
-    regmodel.registerExistsAndHasValue(TAS5822::Register::DEVICE_CTRL_2, 0b00000011);
+    // check that begin writes to some registers
+    EXPECT_GT(regmodel.getTotalRegisterWriteCount(), 0);
+
+    // check registers have expected values
+    testRegisterHasValue(regmodel, TAS5822::Register::DEVICE_CTRL_2, 0b00000011);
 }
 
 TEST_F(RegisterModelTest, MuteCommandWorks) {
@@ -151,7 +180,8 @@ TEST_F(RegisterModelTest, MuteCommandWorks) {
 
     // verify setMuted(true) sets correct bit to correct value
     amp.setMuted(true);
-    regmodel.registerExistsAndHasValue(
+    testRegisterHasValue(
+        regmodel,
         TAS5822::Register::DEVICE_CTRL_2,
         0b00001000, /* Expect Mute bit is HIGH */
         0b00001000  /* Mute bit bit-mask */
@@ -159,14 +189,16 @@ TEST_F(RegisterModelTest, MuteCommandWorks) {
 
     // verify setMuted(false) sets correct bit to correct value
     amp.setMuted(false);
-    regmodel.registerExistsAndHasValue(
+    testRegisterHasValue(
+        regmodel,
         TAS5822::Register::DEVICE_CTRL_2,
         0b00000000, /* Expect Mute bit is LOW */
         0b00001000  /* Mute bit bit-mask */
     );
 
     // verify no other bits were changed by previous operations
-    regmodel.registerExistsAndHasValue(
+    testRegisterHasValue(
+        regmodel,
         TAS5822::Register::DEVICE_CTRL_2,
         0xFF,      /* Expect all other bits have original value */
         0b11110111 /* Inverted Mute bit bit-mask */
@@ -199,7 +231,7 @@ TEST_F(RegisterModelTest, AnalogGainCalculation) {
         amp.setAnalogGain(result.first);
 
         // Check that AGAIN register matches expected value
-        regmodel.registerExistsAndHasValue(TAS5822::Register::AGAIN, static_cast<uint8_t>(result.second));
+        testRegisterHasValue(regmodel, TAS5822::Register::AGAIN, static_cast<uint8_t>(result.second));
     }
 }
 
